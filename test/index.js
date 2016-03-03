d7f08a0f5ef788120c1f7fcab44bf1ca8c8579e9
Oliver White <oliver.white@MOO-00157.local>
2016-03-03 12:30:55 +0000

    configure babelrc

--- test/index.js
+++ test/index.js
@@ -4,14 +4,25 @@ import reducers from '../reducers'
 import * as actions from '../actions'
 
 describe('Redux Reducers', () => {
+
     describe('Frames', () => {
+
         describe('#position', () => {
+
             it('moves position forward', () => {
+                // const text = 'Finish docs'
+                // const expectedAction = {
+                //     type: types.ADD_TODO,
+                //     text
+                // }
+                // expect(actions.addTodo(text)).toEqual(expectedAction)
                 return true
             })
+
             it('moves position backward', () => {
                 return true
             })
+
         })
     })
 })
