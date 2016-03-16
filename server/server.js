94845cbecbc5098c49b6890f154dff6184d04279
Oliver White <oliver.white@MOO-00157.local>
2016-03-16 09:31:55 +0000

    allow access from all origins

--- server/server.js
+++ server/server.js
@@ -13,6 +13,13 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS, HEAD');
+    res.header('Access-Control-Allow-Headers', 'X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization');
+    next();
+});
+
 app.get('/api/animation', function (req, res) {
 	res.status(200).send('all');
 });
